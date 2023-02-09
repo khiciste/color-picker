6da7e5980594b241f13bcb4a4d73be3c2fc1e75e
kc <kevin.cleland@gmail.com>
2023-02-08 21:59:23 -0500

    first commit

---
+++ src/ColorPicker/ColorPicker.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './ColorPicker.css';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
+export default function ColorPicker() {
+  const [color, setColor] = useState('Tomato');
+
+ const divStyle = {backgroundColor: color};
+
+  return (
+    <div style={divStyle}>
+      <h1>Selected color: {color}</h1>
+      <div id="color-buttons">
+        {colorNames.map((colorName)=>(
+          <button 
+            onClick={() => setColor(colorName)} 
+            key={colorName}>
+              {colorName}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
